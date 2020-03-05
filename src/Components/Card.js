d0db46a1e0dba376a8eb426413b67a9f738e5b36
Austen <connect@austenvalentine.com>
2020-03-05 12:41:36 -0500

    fixed animation with conditional rendering

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,25 +2,29 @@ import React, { useState, useEffect } from "react";
 import "./Card.css";
 
 function Card({ countries, changeRegion }) {
+  const PLAY = "PLAY";
+  const STOP = "STOP";
+
   const [country, setCountry] = useState(null);
+  const [animation, setAnimation] = useState(PLAY);
 
-  useEffect(
-    function() {
-      const randomIndex = Math.floor(Math.random() * countries.length);
-      setCountry(countries[randomIndex]);
-    },
-    [countries]
-  );
+  useEffect(() => {
+    setCountry(countries[Math.floor(Math.random() * countries.length)]);
+    setTimeout(() => {
+      setAnimation(STOP);
+    }, 4000);
+  }, [countries]);
 
-  function pickNextCountry() {
-    const randomIndex = Math.floor(Math.random() * countries.length);
-    setCountry(countries[randomIndex]);
-
-    const animated = document.querySelector(".animated");
-    animated.classList.remove(".animated");
-    const animatedCopy = animated.cloneNode(true);
-    animated.parentNode.replaceChild(animatedCopy, animated);
-    animatedCopy.classList.add(".animated");
+  function handleNextClick() {
+    if (animation === PLAY) {
+      return null;
+    }
+    setCountry(countries[Math.floor(Math.random() * countries.length)]);
+    setAnimation(PLAY);
+    setTimeout(() => {
+      setAnimation(STOP);
+      console.log(animation);
+    }, 4000);
   }
 
   return (
@@ -31,19 +35,37 @@ function Card({ countries, changeRegion }) {
           the capital of
         </p>
       </div>
-      <div className="animated">
-        <div className="answer">
-          <p className="country-name">{country && country.name}</p>
-          <div className="country-flag">
+      <div className="answer">
+        {country && animation === PLAY && (
+          <span>
+            <p className="country-name country-name-play">
+              {country && country.name}
+            </p>
+            <div className="country-flag country-flag-play">
+              <img
+                src={country && country.flag}
+                alt={`flag of ${country && country.name}`}
+              ></img>
+            </div>
+            <div className="progress-bar progress-play"></div>
+          </span>
+        )}
+      </div>
+      {country && animation === STOP && (
+        <span>
+          <p className="country-name country-name-stop">
+            {country && country.name}
+          </p>
+          <div className="country-flag country-name-stop">
             <img
               src={country && country.flag}
               alt={`flag of ${country && country.name}`}
             ></img>
           </div>
-        </div>
-        <div className="progress-bar"></div>
-      </div>
-      <button onClick={pickNextCountry}>Next Capital</button>
+          <div className="progress-bar progress-stop"></div>
+        </span>
+      )}
+      <button onClick={handleNextClick}>Next Capital</button>
       <button onClick={changeRegion}>Change region</button>
     </div>
   );
